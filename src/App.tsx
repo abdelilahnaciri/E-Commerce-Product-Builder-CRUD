8db04c98c0aeb1d084d615f02680210a4228187b
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2024-09-19 18:45:24 +0100

    create image & button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import "./App.css";
+import React from "react";
+import ProductCard from "./components/ProductCard";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h3 className="text-3xl font-bold underline">Hello World!</h3>
+    <div className=" m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 rounded-md">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
     </div>
   );
-}
+};
 
 export default App;
