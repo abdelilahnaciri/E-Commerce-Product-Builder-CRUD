cb989d731146627ad2da71a81e5c3f8cf99c5052
Abdelilah Naciri <abnaciri@e3r3p7.1337.ma>
2024-09-22 18:52:47 +0100

    Enhance Input, Button, and Modal form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import ProductCard from "./components/ProductCard";
-import { productList } from "./data";
+import { formInputsList, productList } from "./data";
 import Modal from "./components/ui/Modal";
 import Button from "./components/ui/Button";
+import Input from "./components/ui/input";
 
 const App = () => {
   /* --------- STATE --------- */
@@ -21,23 +22,36 @@ const App = () => {
   const renderProductList = productList.map((product) => (
     <ProductCard key={product.id} product={product} />
   ));
+  const renderFormInputList = formInputsList.map((input) => (
+    <div className="flex flex-col">
+      <label
+        htmlFor={input.id}
+        className="mb-[2px] text-sm font-medium text-gray-700"
+      >
+        {input.label}
+      </label>
+      <Input type="text" id={input.id} name={input.name} />
+    </div>
+  ));
 
   return (
     <main className="container">
-      <Button
-        className="bg-indigo-700 hover:bg-indigo-800"
-        onClick={() => setIsOpen(true)}
-      >
+      <Button className="bg-indigo-700 hover:bg-indigo-800" onClick={openModal}>
         Add
       </Button>
       <div className=" m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 p-2 rounded-md">
         {renderProductList}
       </div>
       <Modal isOpen={isOpen} closeModal={closeModal} title="ADD A NEW PRODUCT">
-        <div className="flex items-center space-x-3">
-          <Button className="bg-indigo-700 hover:bg-indigo-800">Cancel</Button>
-          <Button className="bg-gray-300 hover:bg-gray-700">Submit</Button>
-        </div>
+        <form className="space-y-3">
+          {renderFormInputList}
+          <div className="flex items-center space-x-3">
+            <Button className="bg-indigo-700 hover:bg-indigo-800">
+              Cancel
+            </Button>
+            <Button className="bg-gray-300 hover:bg-gray-700">Submit</Button>
+          </div>
+        </form>
       </Modal>
     </main>
   );
